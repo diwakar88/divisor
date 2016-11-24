18928d850c7d1a9b3f19044961cc917195a1bf3f
Diwakar kandel <damodarkandel435@gmail.com>
2016-11-24 10:24:47 +0545

    Add files via upload
    Use divisor as 10.

---
+++ rim.c
@@ -0,0 +1,22 @@
+/*
+Student Name:Diwakar kandel
+Subject:programming fundamental
+Roll No:08
+Lab No:
+Program:WAP to print the remainder of the number asssigned by user.
+Date:24nov,2016
+*/
+#include<stdio.h>
+int main()
+{
+ int n,d,rim;
+ printf("Enter any nos:\n");
+ scanf("%d",&n);
+  printf("Enter any divisor:\n");
+ scanf("%d",&d);
+ rim=n%d;
+ printf("ask user to enter divisor of %d",rim);
+ 
+ return 0;
+ }
+ 
