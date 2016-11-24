7758d34b2ccbfa163de96c99e71aac79c580140b
Diwakar kandel <damodarkandel435@gmail.com>
2016-11-24 10:25:28 +0545

    Add files via upload
    Ask user to enter divisor.

---
+++ rim n.c
@@ -0,0 +1,20 @@
+/*
+Student Name:Diwakar kandel
+Subject:programming fundamental
+Roll No:08
+Lab No:
+Program:WAP to print the remainder of the number asssigned by user.
+Date:24nov,2016
+*/
+#include<stdio.h>
+int main()
+{
+int n,rim;
+printf("Enter any nos:\n");
+scanf("%d",& n);
+
+ rim=n%10;
+ printf("\n reminder of %d",rim);
+ 
+ return 0;
+ }
